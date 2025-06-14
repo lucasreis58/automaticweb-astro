14a034eaf027edb3107d637d0be3e9d31f0e2212
lucasreis58 <lucasreisn8n@gmail.com>
2025-06-14 00:02:06 -0300

    Ajusta configuração da integração Decap CMS para compatibilidade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import decap from "astro-decap-cms";
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [decap()],
+    integrations: [
+        decap({
+            // Esta opção desabilita a injeção automática do script de login,
+            // o que evita o erro de compatibilidade com o Astro v5.
+            disableIdentityWidgetInjection: true,
+        }),
+    ],
 });
